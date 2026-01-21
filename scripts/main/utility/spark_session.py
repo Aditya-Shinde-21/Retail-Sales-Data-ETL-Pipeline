02536350693279db2e7e7093df99cd5512d2aa8f
Aditya-Shinde-21 <139449660+Aditya-Shinde-21@users.noreply.github.com>
2026-01-21 21:21:11 +0530

    Update spark_session.py

--- scripts/main/utility/spark_session.py
+++ scripts/main/utility/spark_session.py
@@ -7,21 +7,24 @@ by spark-submit, in Airflow dag.
 """
 
 def get_spark_session():
-    spark = (
-        SparkSession.builder
-        .appName("Sales-Data-ETL-Optimized")
-        .config("spark.sql.shuffle.partitions", "6")
-        .config("spark.default.parallelism", "6")
+    try:
+        spark = (
+            SparkSession.builder
+            .appName("Sales-Data-ETL-Optimized")
+            .config("spark.sql.shuffle.partitions", "6")
+            .config("spark.default.parallelism", "6")
 
-        # Spark UI persistence
-        .config("spark.eventLog.enabled", "true")
-        .config("spark.eventLog.dir", "file:///mnt/filepath/to/spark-event-logs")
-        .config("spark.history.fs.logDirectory", "file:///mnt/filepath/to/spark-event-logs")
+            # Spark UI persistence
+            .config("spark.eventLog.enabled", "true")
+            .config("spark.eventLog.dir", "file:///mnt/filepath/to/spark-event-logs")
+            .config("spark.history.fs.logDirectory", "file:///mnt/filepath/to/spark-event-logs")
 
-        # MySQL connector jar
-        .config("spark.driver.extraClassPath",
-        "/mnt/filepath/to/mysql-connector-j-9.5.0.jar")
-        .getOrCreate()
-    )
+            # MySQL connector jar
+            .config("spark.driver.extraClassPath",
+                    "/mnt/filepath/to/mysql-connector-j-9.5.0.jar")
+            .getOrCreate()
+        )   
+    except Exception as e:
+        raise e
 
     return spark
