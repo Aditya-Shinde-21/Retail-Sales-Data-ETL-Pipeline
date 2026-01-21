e41496418ebede3727dd4f516fd0c871d10cf2cd
Aditya-Shinde-21 <139449660+Aditya-Shinde-21@users.noreply.github.com>
2026-01-21 20:07:19 +0530

    Update upload_file_to_s3.py

--- scripts/generate_data/upload_file_to_s3.py
+++ scripts/generate_data/upload_file_to_s3.py
@@ -1,25 +1,27 @@
 import os
-from resources.dev import config
-from scripts.main.utility.s3_client_object import *
-from scripts.main.utility.encrypt_decrypt import *
+import boto3
 
-
-def upload_to_s3(s3_directory, s3_bucket, local_file_path):
+def upload_to_s3(s3_client, s3_directory, s3_bucket, local_file_path):
+    s3_prefix = f"{s3_directory}"
     try:
-        # get s3 client
-        s3_client_provider = S3ClientProvider(decrypt(config.aws_access_key), decrypt(config.aws_secret_key))
-        s3_client = s3_client_provider.get_client()
-        
         for root, dirs, files in os.walk(local_file_path):
             for file in files:
-                print(file)
                 local_file_path = os.path.join(root, file)
-                s3_key = f"{s3_directory}{file}"
+                s3_key = f"{s3_prefix}{file}"
+
+                print(f"Uploading: {file}")
                 s3_client.upload_file(local_file_path, s3_bucket, s3_key)
-                print(f"Uploaded file to {s3_bucket}/{s3_key}")
-                
+                print(f"Uploaded {file} to {s3_bucket}/{s3_key}")
+
     except Exception as e:
         raise e
 
-local_file_path = "filepath to generated csv files"
-upload_to_s3(config.s3_source_directory, config.bucket_name, local_file_path)
+aws_access_key = input("Enter AWS Access Key: ")
+aws_secret_key = input("Enter AWS Secret Key: ")
+s3_client = boto3.Session(aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key).client('s3')
+
+local_file_path = "D:\\DE_Project_Files\\sales_data_to_s3\\"
+s3_directory = "sales_data/"
+s3_bucket = "customer-sales-data-project"
+
+upload_to_s3(s3_client, s3_directory, s3_bucket, local_file_path)
