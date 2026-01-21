4de930449def9abc3a032e886f8b5ad45c53bb3e
Aditya-Shinde-21 <139449660+Aditya-Shinde-21@users.noreply.github.com>
2026-01-21 20:40:18 +0530

    Update spark_session.py

--- scripts/main/utility/spark_session.py
+++ scripts/main/utility/spark_session.py
@@ -1,27 +1,27 @@
-from pyspark.sql import *
-from resources.dev import config
-from scripts.main.utility.encrypt_decrypt import decrypt
+from pyspark.sql import SparkSession
 
-def spark_session():
-    spark = SparkSession.builder.master("local[4]")\
-        .appName("test_run") \
+"""
+Creates and returns a SparkSession.
+Spark execution mode (local / cluster) is controlled externally
+by spark-submit, in Airflow dag.
+"""
+
+def get_spark_session():
+    spark = (
+        SparkSession.builder
+        .appName("Sales-Data-ETL-Optimized")
+        .config("spark.sql.shuffle.partitions", "6")
+        .config("spark.default.parallelism", "6")
+
+        # Spark UI persistence
+        .config("spark.eventLog.enabled", "true")
+        .config("spark.eventLog.dir", "file:///mnt/filepath/to/spark-event-logs")
+        .config("spark.history.fs.logDirectory", "file:///mnt/filepath/to/spark-event-logs")
+
+        # MySQL connector jar
         .config("spark.driver.extraClassPath",
-        "<filepath-to-mysql-connector-jar>") \
-        .config("spark.hadoop.fs.s3a.access.key", decrypt(config.aws_access_key))\
-        .config("spark.hadoop.fs.s3a.secret.key", decrypt(config.aws_secret_key))\
-        .config("spark.hadoop.fs.s3a.endpoint", f"s3.{config.region}.amazonaws.com")\
-        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
-        .config("spark.hadoop.fs.s3a.path.style.access", "true")\
-        .config("spark.driver.memory", "4g") \
-        .config("spark.driver.memoryOverhead", "512m")\
-        .config("spark.sql.shuffle.partitions", "8")\
-        .config("spark.default.parallelism", "8")\
+        "/mnt/filepath/to/mysql-connector-j-9.5.0.jar")
         .getOrCreate()
+    )
 
     return spark
-
-if __name__ == "__main__":
-    spark = spark_session()
-    print("Created spark session:", spark)
-
-    spark.stop()
